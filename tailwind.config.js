7a3374f11eae2a6bda3947e80f1cc7be1bacbc27
mario <mamanzasanchez@gmail.com>
2024-05-11 20:53:06 -0600

    article grid

--- tailwind.config.js
+++ tailwind.config.js
@@ -10,11 +10,11 @@ module.exports = {
 
       colors: {
         "brigth-red": "hs1(12,88%,59%)",
-        "Dark Blue": "hsl(228, 39%, 23%)",
-        "Dark Grayish Blue": "hsl(227, 12%, 61%)",
-        "Very Dark Blue": "hsl(233, 12%, 13%)",
-        "Very Pale Red": "hsl(13, 100%, 96%)",
-        "Very Light Gray": "hsl(0, 0%, 98%)"
+        "Dark-Blue": "hsl(228, 39%, 23%)",
+        "Dark-Grayish-Blue": "hsl(227, 12%, 61%)",
+        "Very-Dark-Blue": "hsl(233, 12%, 13%)",
+        "Very-Pale-Red": "hsl(13, 100%, 96%)",
+        "Very-Light-Gray": "hsl(0, 0%, 98%)"
       },
       backgroundImage: {
         "close-menu": "url('../images/icon-close.svg')",
